47c1f694166d32b99ba15083883bc49d875f47b2
Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>
2019-02-26 15:38:28 +0100

    Add comments

---
+++ script/R/velocity.R
@@ -0,0 +1,168 @@
+#!/usr/bin/Rscript
+#rm(list=ls(all=TRUE)) - clear all in Environment
+
+####################################
+#  Documentation
+####################################
+# Created: 26/02/2019
+# This script aggregate and plot data (Velocity)
+# GitHub repo (private): https://github.com/atsukotominaga/expertpiano/tree/master/script/R 
+
+####################################
+#  Requirements
+####################################
+# !!! Set working directory to file source location !!!
+
+# Install and load required packages
+if (!require("dplyr")) {install.packages("dplyr"); require("dplyr")}
+if (!require("ggplot2")) {install.packages("ggplot2"); require("ggplot2")}
+
+# Create necessary folders if not exist
+# plot
+if (!file.exists("plot")){
+  dir.create("plot")
+}
+# velocity folder
+if (!file.exists("plot/png/")){
+  dir.create("plot/velocity")
+}
+
+####################################
+### Velocity - dynamics
+####################################
+# Calculate Acc (acceleration - velocity difference between notes)
+df_vel <- df_analysis %>% dplyr::filter(Key_OnOff == 1)
+df_vel$Acc <- diff(c(0, df_vel$Velocity))
+
+# Remove the first note for pilot data
+df_vel_acc <- data.frame()
+for (subnr in unique(df_vel$SubNr)){
+  for (block in unique(df_vel$BlockNr[df_vel$SubNr == subnr])){
+    for (trial in unique(df_vel$TrialNr[df_vel$SubNr == subnr & df_vel$BlockNr == block])){
+      df_current <- df_vel %>% dplyr::filter(SubNr == subnr & BlockNr == block & TrialNr == trial)
+      df_vel_acc <- rbind(df_vel_acc, subset(df_current, df_current$NoteNr != min(df_current$NoteNr)))
+    }
+  }
+}
+
+# Remove the first note
+df_vel_acc <- df_vel %>% dplyr::filter(NoteNr != 17)
+
+# Assign a sequence number for each tone
+df_vel$Note <- rep(1:67, length(df_vel$NoteNr)/67) # for vel_seq
+df_vel_acc$Interval <- rep(1:66, length(df_vel_acc$NoteNr)/66) # for vel_acc_seq
+
+# Assign numbers for forte and piano
+ls_forte <- list(c(1:8), c(17:24), c(42:49), c(58:65))
+ls_piano <- list(c(9:16), c(25:32), c(34:41), c(50:57))
+
+# Forte
+df_vel$Dynamics <- NA
+for (phrase in 1:length(ls_forte)){
+  for (note in 1:length(ls_forte[[phrase]])){
+    df_vel$Dynamics[df_vel$Skill == 'dynamics' & df_vel$Note == ls_forte[[phrase]][note]] <- 'Forte'
+  }
+}
+
+# Piano
+for (phrase in 1:length(ls_piano)){
+  for (note in 1:length(ls_piano[[phrase]])){
+    df_vel$Dynamics[df_vel$Skill == 'dynamics' & df_vel$Note == ls_piano[[phrase]][note]] <- 'Piano'
+  }
+}
+
+# Average data
+# Overall average
+vel <- aggregate(Velocity~Condition*Skill, data = subset(df_vel, df_ioi$Note != 33 & df_ioi$Note != 66 & df_ioi$Note != 67), 
+                 FUN = function(x){c(N = length(x), mean = mean(x), sd = sd(x), sem = sd(x)/sqrt(length(x)))})
+
+# Average for forte and piano
+vel_dyn <- aggregate(Velocity~Condition*Skill*Dynamics, data = df_vel,
+                     FUN = function(x){c(N = length(x), mean = mean(x), sd = sd(x), sem = sd(x)/sqrt(length(x)))})
+
+# Average for each note
+vel_seq <- aggregate(Velocity~Note*Condition*Skill, data = df_vel, 
+                     FUN = function(x){c(N = length(x), mean = mean(x), sd = sd(x), sem = sd(x)/sqrt(length(x)))})
+vel_seq[,4][vel_seq$Note == 33 | vel_seq$Note == 66 | vel_seq$Note == 67] <- NA
+vel_acc_seq <- aggregate(Acc~Interval*Condition*Skill, data = df_vel_acc, 
+                         FUN = function(x){c(N = length(x), mean = mean(x), sd = sd(x), sem = sd(x)/sqrt(length(x)))})
+vel_acc_seq[,4][vel_acc_seq$Interval == 32 | vel_acc_seq$Interval == 33 | vel_acc_seq$Interval == 65 | vel_acc_seq$Interval == 66] <- NA
+
+# Aggregated data
+vel <- cbind(vel, as.data.frame(vel[,3]))
+vel_dyn <- cbind(vel_dyn, as.data.frame(vel_dyn[,4]))
+vel_seq <- cbind(vel_seq, as.data.frame(vel_seq[,4]))
+vel_acc_seq <- cbind(vel_acc_seq, as.data.frame(vel_acc_seq[,4]))
+
+# Add a grouping name
+for (cond in 1:length(ls_grouping$Condition)){
+  for (skill in 1:length(ls_grouping$Skill)){
+    vel$Grouping[vel$Condition == ls_grouping$Condition[cond] & vel$Skill == ls_grouping$Skill[skill]] <- 
+      paste(ls_grouping$Condition[cond], '-', ls_grouping$Skill[skill], sep = '')
+    vel_seq$Grouping[vel_seq$Condition == ls_grouping$Condition[cond] & vel_seq$Skill == ls_grouping$Skill[skill]] <- 
+      paste(ls_grouping$Condition[cond], '-', ls_grouping$Skill[skill], sep = '')
+    vel_acc_seq$Grouping[vel_acc_seq$Condition == ls_grouping$Condition[cond] & vel_acc_seq$Skill == ls_grouping$Skill[skill]] <- 
+      paste(ls_grouping$Condition[cond], '-', ls_grouping$Skill[skill], sep = '')
+  }
+}
+
+####################################
+### Velocity plots
+####################################
+plot_vel <- ggplot(data = vel, aes(x = Skill, y = mean, fill = Condition)) +
+  geom_bar(stat = "identity", position = position_dodge()) +
+  geom_errorbar(aes(ymin = mean - sem, ymax = mean + sem),
+                width = .2, position=position_dodge(.9)) + 
+  labs(y = "Velocity (0-127)") + coord_cartesian(ylim = c(20, 80)) + 
+  theme_classic()
+
+plot_vel_dyn <- ggplot(data = vel_dyn, aes(x = Dynamics, y = mean, fill = Condition)) +
+  geom_bar(stat = "identity", position = position_dodge()) +
+  geom_errorbar(aes(ymin = mean - sem, ymax = mean + sem),
+                width=.2, position = position_dodge(.9)) +
+  labs(y = "Velocity (0-127)") + #coord_cartesian(ylim = c(0, 95)) + 
+  theme_classic()
+
+plot_vel_seq <- ggplot(data = vel_seq, aes(x = Note, y = mean, group = Grouping, shape = Grouping, colour = Grouping)) +
+  geom_line() +
+  geom_point() +
+  geom_hline(data = vel, aes(yintercept = mean, colour = Grouping), linetype = 'dashed') + # Mean Velocity for each grouping
+  geom_errorbar(aes(ymin = mean - sem, ymax = mean + sem), width=.2,
+                position = position_dodge(0.05)) + 
+  labs(y = "Velocity (0-127)") + scale_x_continuous(breaks=seq(1,67,1)) +
+  theme_classic()
+
+plot_vel_seq_f <- ggplot(data = vel_seq, aes(x = Note, y = mean, group = Condition, shape = Condition, colour = Condition)) +
+  geom_line() +
+  geom_point() +
+  facet_grid(Skill ~ .) +
+  geom_errorbar(aes(ymin = mean - sem, ymax = mean + sem), width=.2,
+                position = position_dodge(0.05)) + 
+  labs(y = "Velocity (0-127)") + scale_x_continuous(breaks=seq(1,67,1)) +
+  theme_classic()
+
+plot_vel_acc_seq <- ggplot(data = vel_acc_seq, aes(x = Interval, y = mean, group = Grouping, shape = Grouping, colour = Grouping)) +
+  geom_line() +
+  geom_point() +
+  geom_errorbar(aes(ymin = mean - sem, ymax = mean + sem), width=.2,
+                position=position_dodge(.05)) + 
+  labs(x = 'Interval', y = "Acceleration") + scale_x_continuous(breaks=seq(1,66,1)) +
+  theme_classic()
+
+plot_vel_acc_seq_f <- ggplot(data = vel_acc_seq, aes(x = Interval, y = mean, group = Condition, shape = Condition, colour = Condition)) +
+  geom_line() +
+  geom_point() +
+  facet_grid(Skill ~ .) +
+  geom_errorbar(aes(ymin = mean - sem, ymax = mean + sem), width=.2,
+                position = position_dodge(.05)) + 
+  labs(x = 'Interval', y = "Acceleration") + scale_x_continuous(breaks=seq(1,66,1)) +
+  theme_classic()
+
+# Save plots
+# png files
+ggsave('./plot/png/plot_vel.png', plot = plot_vel, dpi = 600, width = 5, height = 4)
+ggsave('./plot/png/plot_vel_dyn.png', plot = plot_vel_dyn, dpi = 600, width = 5, height = 4)
+ggsave('./plot/png/plot_vel_seq.png', plot = plot_vel_seq, dpi = 600, width = 15, height = 4)
+ggsave('./plot/png/plot_vel_seq_f.png', plot = plot_vel_seq_f, dpi = 600, width = 15, height = 4)
+ggsave('./plot/png/plot_vel_acc_seq.png', plot = plot_vel_acc_seq, dpi = 600, width = 15, height = 4) 
+ggsave('./plot/png/plot_vel_acc_seq_f.png', plot = plot_vel_acc_seq_f, dpi = 600, width = 15, height = 4) 
