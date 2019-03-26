ce67bbb8a636db2011e3fae62a3e673275037cb9
Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>
2019-03-26 21:30:50 +0100

    Add posthoc test and export stats results

--- script/R/kot.R
+++ script/R/kot.R
@@ -193,6 +193,11 @@ kot_sub <- aggregate(KOT~Condition*Skill*SubSkill, data = subset(df_trim_kot, df
                                                                       df_trim_kot$Interval != 41 & df_trim_kot$Interval != 49 & df_trim_kot$Interval != 57),
                         FUN = function(x){c(N = length(x), mean = mean(x), sd = sd(x), sem = sd(x)/sqrt(length(x)))})
 
+kot_firstEnd <- aggregate(KOT~Condition*Skill*SubSkill, data = subset(df_trim_kot, df_trim_kot$Interval == 1 | df_trim_kot$Interval == 7 | df_trim_kot$Interval == 9 | df_trim_kot$Interval == 15 | df_trim_kot$Interval == 17 | df_trim_kot$Interval == 23
+                                                                      | df_trim_kot$Interval == 25 | df_trim_kot$Interval == 31 | df_trim_kot$Interval == 34 | df_trim_kot$Interval == 40 | df_trim_kot$Interval == 42 | df_trim_kot$Interval == 48
+                                                                      | df_trim_kot$Interval == 50 | df_trim_kot$Interval == 56 | df_trim_kot$Interval == 58 | df_trim_kot$Interval == 64),
+                     FUN = function(x){c(N = length(x), mean = mean(x), sd = sd(x), sem = sd(x)/sqrt(length(x)))})
+
 # Average for each note
 kot_seq <- aggregate(KOT~Interval*Condition*Skill, data = df_trim_kot, 
                      FUN = function(x){c(N = length(x), mean = mean(x), sd = sd(x), sem = sd(x)/sqrt(length(x)))})
@@ -220,6 +225,7 @@ kot_var_trial <- aggregate(Variability~TrialNr*Condition*Skill, data = df_var,
 kot <- cbind(kot, as.data.frame(kot[,3]))
 kot_change <- cbind(kot_change, as.data.frame(kot_change[,4]))
 kot_sub <- cbind(kot_sub, as.data.frame(kot_sub[,4]))
+kot_firstEnd <- cbind(kot_firstEnd, as.data.frame(kot_firstEnd[,4]))
 kot_seq <- cbind(kot_seq, as.data.frame(kot_seq[,4]))
 kot_var <- cbind(kot_var, as.data.frame(kot_var[,3]))
 kot_var_trial <- cbind(kot_var_trial, as.data.frame(kot_var_trial[,4]))
@@ -233,6 +239,8 @@ for (cond in 1:length(ls_grouping$Condition)){
     kot_change$Grouping[kot_change$Condition == ls_grouping$Condition[cond] & kot_change$Skill == ls_grouping$Skill[skill]] <-
       paste(ls_grouping$Condition[cond], '-', ls_grouping$Skill[skill], sep = '')
     kot_sub$Grouping[kot_sub$Condition == ls_grouping$Condition[cond] & kot_sub$Skill == ls_grouping$Skill[skill]] <-
+      paste(ls_grouping$Condition[cond], '-', ls_grouping$Skill[skill], sep = '')
+    kot_firstEnd$Grouping[kot_firstEnd$Condition == ls_grouping$Condition[cond] & kot_firstEnd$Skill == ls_grouping$Skill[skill]] <-
       paste(ls_grouping$Condition[cond], '-', ls_grouping$Skill[skill], sep = '')
     kot_seq$Grouping[kot_seq$Condition == ls_grouping$Condition[cond] & kot_seq$Skill == ls_grouping$Skill[skill]] <-
       paste(ls_grouping$Condition[cond], '-', ls_grouping$Skill[skill], sep = '')
@@ -248,6 +256,8 @@ kot_change$Order[kot_change$Skill == "articulation"] <- 1
 kot_change$Order[kot_change$Skill == "dynamics"] <- 2
 kot_sub$Order[kot_sub$Skill == "articulation"] <- 1
 kot_sub$Order[kot_sub$Skill == "dynamics"] <- 2
+kot_firstEnd$Order[kot_firstEnd$Skill == "articulation"] <- 1
+kot_firstEnd$Order[kot_firstEnd$Skill == "dynamics"] <- 2
 
 ####################################
 ### Plots
@@ -275,6 +285,14 @@ p_kot_sub <- ggplot(data = kot_sub, aes(x = reorder(SubSkill, Order), y = mean, 
   labs(x = "SubSkill", y = "Mean KOT (ms)") + #coord_cartesian(ylim = c(100, 250)) +
   theme_classic()
 p_kot_sub
+
+p_kot_firstEnd <- ggplot(data = kot_firstEnd, aes(x = reorder(SubSkill, Order), y = mean, fill = Condition)) +
+  geom_bar(stat = "identity", position = position_dodge()) +
+  geom_errorbar(aes(ymin = mean - sem, ymax = mean + sem),
+                width=.2, position = position_dodge(.9)) +
+  labs(x = "SubSkill", y = "Mean KOT (ms)") + #coord_cartesian(ylim = c(100, 250)) +
+  theme_classic()
+p_kot_firstEnd
 
 p_kot_seq <- ggplot(data = kot_seq, aes(x = Interval, y = mean, group = Condition, shape = Condition, colour = Condition)) +
   geom_line() +
@@ -306,7 +324,9 @@ p_kot_var_trial
 # Save plots
 # png files
 ggsave("./plot/kot/p_kot.png", plot = p_kot, dpi = 600, width = 5, height = 4)
+ggsave("./plot/kot/p_kot_change.png", plot = p_kot_change, dpi = 600, width = 5, height = 4)
 ggsave("./plot/kot/p_kot_sub.png", plot = p_kot_sub, dpi = 600, width = 5, height = 4)
+ggsave("./plot/kot/p_kot_firstEnd.png", plot = p_kot_firstEnd, dpi = 600, width = 5, height = 4)
 ggsave("./plot/kot/p_kot_seq.png", plot = p_kot_seq, dpi = 600, width = 15, height = 4)
 ggsave("./plot/kot/p_kot_var.png", plot = p_kot_var, dpi = 600, width = 5, height = 4)
 ggsave("./plot/kot/p_kot_var_trial.png", plot = p_kot_var_trial, dpi = 600, width = 10, height = 4)
@@ -325,11 +345,13 @@ kot_anova <- ezANOVA(
   , detailed = TRUE
 )
 print(kot_anova)
+write.csv(kot_anova$ANOVA, file = "./stats/kot_anova.csv")
 
+# posthoc comparison
 kot_posthoc <- aov(KOT~Condition*Skill, data = df_trim_kot)
-print(TukeyHSD(kot_posthoc))
+kot_posthoc <-TukeyHSD(kot_posthoc)
+write.csv(kot_posthoc$`Condition:Skill`, file = "./stats/kot_posthoc.csv")
 
-# Two-way ANOVA
 # kot_change
 kot_change_anova <- ezANOVA(
   data = subset(df_trim_kot, df_trim_kot$Interval == 8 | df_trim_kot$Interval == 16 | df_trim_kot$Interval == 24 | 
@@ -341,6 +363,14 @@ kot_change_anova <- ezANOVA(
   , detailed = TRUE
 )
 print(kot_change_anova)
+write.csv(kot_change_anova$ANOVA, file = "./stats/kot_change_anova.csv")
+write.csv(kot_change_anova$`Mauchly's Test for Sphericity`, file = "./stats/kot_change_anova_mau.csv")
+write.csv(kot_change_anova$`Sphericity Corrections`, file = "./stats/kot_change_anova_sph.csv")
+
+# posthoc comparison
+kot_change_posthoc <- aov(KOT~Condition*Skill, data = df_trim_kot)
+kot_change_posthoc <-TukeyHSD(kot_change_posthoc)
+write.csv(kot_change_posthoc$`Condition:Skill`, file = "./stats/kot_change_posthoc.csv")
 
 # kot_sub
 kot_sub_anova <- ezANOVA(
@@ -353,24 +383,37 @@ kot_sub_anova <- ezANOVA(
   , detailed = TRUE
 )
 print(kot_sub_anova)
+write.csv(kot_sub_anova$ANOVA, file = "./stats/kot_sub_anova.csv")
+write.csv(kot_sub_anova$`Mauchly's Test for Sphericity`, file = "./stats/kot_sub_anova_mau.csv")
+write.csv(kot_sub_anova$`Sphericity Corrections`, file = "./stats/kot_sub_anova_sph.csv")
 
 kot_sub_posthoc <- aov(KOT~Condition*SubSkill, data = subset(df_trim_kot, df_trim_kot$Interval != 8 & df_trim_kot$Interval != 16 & df_trim_kot$Interval != 24 & 
                                                                df_trim_kot$Interval != 41 & df_trim_kot$Interval != 49 & df_trim_kot$Interval != 57))
-print(TukeyHSD(kot_sub_posthoc))
+kot_sub_posthoc <- TukeyHSD(kot_sub_posthoc)
+write.csv(kot_sub_posthoc$`Condition:SubSkill`, file = "./stats/kot_sub_posthoc.csv")
 
-# # kot_var
-# kot_var_anova <- ezANOVA(
-#   data = df_var[complete.cases(df_var),]
-#   , dv = .(Variability)
-#   , wid = .(SubNr)
-#   , within = .(Condition, Skill)
-#   , type = 3
-#   , detailed = TRUE
-# )
-# print(kot_var_anova)
-# 
-# kot_var_posthoc <- aov(Variability~Condition*Skill, data = df_var[complete.cases(df_var),])
-# print(TukeyHSD(kot_var_posthoc))
+# kot_firstEnd
+kot_firstEnd_anova <- ezANOVA(
+  data = subset(df_trim_kot, df_trim_kot$Interval == 1 | df_trim_kot$Interval == 7 | df_trim_kot$Interval == 9 | df_trim_kot$Interval == 15 | df_trim_kot$Interval == 17 | df_trim_kot$Interval == 23
+                | df_trim_kot$Interval == 25 | df_trim_kot$Interval == 31 | df_trim_kot$Interval == 34 | df_trim_kot$Interval == 40 | df_trim_kot$Interval == 42 | df_trim_kot$Interval == 48
+                | df_trim_kot$Interval == 50 | df_trim_kot$Interval == 56 | df_trim_kot$Interval == 58 | df_trim_kot$Interval == 64)
+  , dv = .(KOT)
+  , wid = .(SubNr)
+  , within = .(Condition, SubSkill)
+  , type = 3
+  , detailed = TRUE
+)
+print(kot_firstEnd_anova)
+write.csv(kot_firstEnd_anova$ANOVA, file = "./stats/kot_firstEnd_anova.csv")
+write.csv(kot_firstEnd_anova$`Mauchly's Test for Sphericity`, file = "./stats/kot_firstEnd_anova_mau.csv")
+write.csv(kot_firstEnd_anova$`Sphericity Corrections`, file = "./stats/kot_firstEnd_anova_sph.csv")
+
+# posthoc comparison
+kot_firstEnd_posthoc <- aov(KOT~Condition*SubSkill, data = subset(df_trim_kot, df_trim_kot$Interval == 1 | df_trim_kot$Interval == 7 | df_trim_kot$Interval == 9 | df_trim_kot$Interval == 15 | df_trim_kot$Interval == 17 | df_trim_kot$Interval == 23
+                                                               | df_trim_kot$Interval == 25 | df_trim_kot$Interval == 31 | df_trim_kot$Interval == 34 | df_trim_kot$Interval == 40 | df_trim_kot$Interval == 42 | df_trim_kot$Interval == 48
+                                                               | df_trim_kot$Interval == 50 | df_trim_kot$Interval == 56 | df_trim_kot$Interval == 58 | df_trim_kot$Interval == 64))
+kot_firstEnd_posthoc <-TukeyHSD(kot_firstEnd_posthoc)
+write.csv(kot_firstEnd_posthoc$`Condition:SubSkill`, file = "./stats/kot_firstEnd_posthoc.csv")
 
 #Normality
 q_plot <- ggplot(subset(df_trim_kot, df_trim_kot$Interval != 8 & df_trim_kot$Interval != 16 & df_trim_kot$Interval != 24 & 
