e95d79235e26f4669b0844b06d5820d41d9f69fd
Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>
2019-11-20 21:51:25 +0100

    edit manual pitch removal
    Signed-off-by: Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>

--- script/R/filter/filtering.R
+++ script/R/filter/filtering.R
@@ -18,10 +18,12 @@ if (!require("editData")) {install.packages("editData"); require("editData")}
 if (!require("ggplot2")) {install.packages("ggplot2"); require("ggplot2")}
 if (!require("wesanderson")) {install.packages("wesanderson"); require("wesanderson")}
 
+# read functions
+source("./function.R")
+
 # setting
 # ggplots
 theme_set(theme_classic())
-theme_update(text = element_text(size = 20, family = "Helvetica Neue LT Std 57 Condensed"), legend.position = "bottom")
 
 # create necessary folders if not exist
 # filtered - all of the outputs will be stored in this folder
@@ -61,143 +63,119 @@ raw_data <- raw_data[c(14, 1:13)]
 ####################################
 # Detect pitch errors
 ####################################
-# 1. FIRST FILTERING (AUTOMATIC)
 # raw_data without metronome
 df_all <- raw_data %>% dplyr::filter(Pitch != 31 & Pitch != 34)
 
 # onset and offset
 df_onset <- df_all %>% dplyr::filter(Key_OnOff == 1)
+df_offset <- df_all %>% dplyr::filter(Key_OnOff == 0)
 
 # calculate IOIs
 df_onset$IOI <- diff(c(0, df_onset$TimeStamp))
 
-# remove the first note
-df_onset_ioi <- df_onset %>% dplyr::filter(NoteNr != 17)
+# 1. FIRST FILTERING (AUTOMATIC)
+### ONSET ###
+# detect pitch errors
+ls_removed_onset <- pitch_remover(df_onset)
 
-# list of pitch errors & missing trials
-ls_error_onset <- list() 
-ls_miss <- list()
-
-# define pitch remove function (pitch_remover)
-for (subnr in unique(df_all$SubNr)){
-  write(sprintf("---SubNr %i---", subnr), file = "./filtered/errormes.txt", append = T) #export the results to a text file
-  print(sprintf("---SubNr %i---", subnr))
-  for (block in c(1:4)){
-    for (trial in c(1:8)){
-      current_onset <- df_onset %>% dplyr::filter(SubNr == subnr & BlockNr == block & TrialNr == trial)
-      if (nrow(current_onset) != 0){ #if current data is not empty
-        if (length(df_ideal$Pitch) != length(current_onset$NoteNr)){ #if # of onsets is not equal to ideal performance
-          ls_error_onset <<- c(ls_error_onset, list(c(subnr, block, trial)))
-          write(sprintf("Onset-NoteNr error - SubNr/BlockNr/TrialNr: %i/%i/%i", subnr, block, trial), file = "./filtered/errormes.txt", append = T)
-          print(sprintf("Onset-NoteNr error - SubNr/BlockNr/TrialNr: %i/%i/%i", subnr, block, trial))
-        } else if (length(df_ideal$Pitch) == length(current_onset$Pitch)) { #if # of onsets and offsets are correct
-          counter = 0 #set a counter so that the following loop will terminate once it detects one pitch error in a trial
-          for (note in 1:length(df_ideal$Pitch)){
-            # detect onset error
-            if (current_onset[note,]$Pitch != df_ideal$Pitch[note]){
-              while (counter == 0){
-                ls_error_onset <<- c(ls_error_onset, list(c(subnr, block, trial)))
-                write(sprintf("Onset-Pitch error - SubNr/BlockNr/TrialNr/NoteNr: %i/%i/%i/%i", subnr, block, trial, note), file = "./filtered/errormes.txt", append = T)
-                print(sprintf("Onset-Pitch error - SubNr/BlockNr/TrialNr/NoteNr: %i/%i/%i/%i", subnr, block, trial, note))
-                counter = counter + 1
-              }
-            }
-          }
-        }
-      } else { #if current data is emtpy"
-        ls_miss <<- c(ls_miss, list(c(subnr, block, trial)))
-        write(sprintf("Missing - SubNr/BlockNr/TrialNr: %i/%i/%i", subnr, block, trial), file = "./filtered/errormes.txt", append = T)
-        print(sprintf("Missing - SubNr/BlockNr/TrialNr: %i/%i/%i", subnr, block, trial))
-      }
-    }
-  }
-}
-
-# Create a list of removed trials
-ls_removed_onset <- c(ls_error_onset, ls_miss)
-# Remove duplication if exists
-ls_removed_onset <- unique(ls_removed_onset)
-# Create a data frame of removed trials
+# create a data frame of removed trials
 df_removed_onset <- data.frame(t(data.frame(ls_removed_onset))) # transpose
 colnames(df_removed_onset) <- c("SubNr", "BlockNr", "TrialNr")
 rownames(df_removed_onset) <- c(1:nrow(df_removed_onset))
 
-# Mark trails with errors by the first filtering
-df_onset$Error1 <- 0
-df_onset_ioi$Error1 <- 0
-for (error in 1:length(ls_removed_onset)){
-  df_onset$Error1[df_onset$SubNr == ls_removed_onset[[error]][1] & df_onset$BlockNr == ls_removed_onset[[error]][2] & df_onset$TrialNr == ls_removed_onset[[error]][3]] <- 1
-  df_onset_ioi$Error1[df_onset_ioi$SubNr == ls_removed_onset[[error]][1] & df_onset_ioi$BlockNr == ls_removed_onset[[error]][2] & df_onset_ioi$TrialNr == ls_removed_onset[[error]][3]] <- 1
-}
+# mark trails with errors by the first filtering
+# df_onset$Error1 <- 0
+# #df_onset_ioi$Error1 <- 0
+# for (error in 1:length(ls_removed_onset)){
+#   df_onset$Error1[df_onset$SubNr == ls_removed_onset[[error]][1] & df_onset$BlockNr == ls_removed_onset[[error]][2] & df_onset$TrialNr == ls_removed_onset[[error]][3]] <- 1
+# }
 
 # 2. SECOND FILTERING (MANUAL)
-# Define fuction (manual)
-manual <- function(data, ideal){
-  counter = 0
-  # define column names
-  if (nrow(data) > length(ideal$Pitch) & nrow(data) < length(ideal$Pitch)*1.05){ # if there are more notes than ideal
-    df_removed_onset$errorType[i] <<- "More"
-    for (note in 1:length(ideal$Pitch)){
-      # detect onset error
-      if (data[note,]$Pitch != ideal$Pitch[note]){
-        while (counter == 0) {
-          df_removed_onset$errorRowNr[i] <<- note
-          print(sprintf("More: Onset-Pitch error - RowNr: %i", note))
-          counter = counter + 1
-        } 
-      }
-    }
-  } else if (nrow(data) > length(ideal$Pitch)*0.95 & nrow(data) < length(ideal$Pitch)){ # if there are less note than ideal
-    df_removed_onset$errorType[i] <<- "Less"
-    for (note in 1:nrow(data)){
-      # detect onset error
-      if (data[note,]$Pitch != ideal$Pitch[note]){
-        while (counter == 0) {
-          df_removed_onset$errorRowNr[i] <<- note
-          print(sprintf("Less: Onset-Pitch error - RowNr: %i", note))
-          counter = counter + 1
-        }
-      }
-    }
-  } else if (nrow(data) == length(ideal$Pitch)){ # there are equal notes to ideal
-    for (note in 1:nrow(data)){
-      df_removed_onset$errorType[i] <<- "Equal"
-      # detect onset error
-      if (data[note,]$Pitch != ideal$Pitch[note]){
-        while (counter == 0) {
-          df_removed_onset$errorRowNr[i] <<- note
-          print(sprintf("Equal: Onset-Pitch error - RowNr: %i", note))
-          counter = counter + 1
-        }
-      }
-    } 
-  } else if (nrow(data) >= length(ideal$Pitch)*1.05){ # error more than 5%
-    df_removed_onset$errorType[i] <<- "Exclude"
-    df_removed_onset$errorRowNr[i] <<- "Exclude the trial (more than 5%)"
-    print("Exclude the trial (more than 5%)")
-  } else if (nrow(data) <= length(ideal$Pitch)*0.95){ # error less than 5%
-    df_removed_onset$errorType[i] <<- "Exclude"
-    df_removed_onset$errorRowNr[i] <<- "Exclude the trial (less than 5%)"
-    print("Exclude the trial (less than 5%)")
-  } else { # other problems
-    df_removed_onset$errorType[i] <<- "Other"
-    df_removed_onset$errorRowNr[i] <<- "Check individually"
+### ONSET ###
+# detect errorType and errorRowNr
+for (i in 1:nrow(df_removed_onset)){
+  current <- df_onset %>% dplyr::filter(SubNr == df_removed_onset[i,1] & BlockNr == df_removed_onset[i,2] & TrialNr == df_removed_onset[i,3])
+  current$RowNr <- c(1:nrow(current))
+  df_removed_onset <- check(df_removed_onset, current, df_ideal)
+}
+
+# manual removal for extra notes
+print("----- Remove extra notes -----")
+removed_more <- subset(df_removed_onset, df_removed_onset$errorType == "More") #only consider extra notes
+df_corrected_more <- data.frame() #create data.frame to store corrected data
+for (i in 1:nrow(removed_more)){
+  current <- df_onset %>% dplyr::filter(SubNr == removed_more[i,1] & BlockNr == removed_more[i,2] & TrialNr == removed_more[i,3])
+  counter <- 0
+  continue <- 1
+  while (continue == 1){
+    print("----- First check -----")
+    current <- manual(removed_more, current)
+    print("----- Correction check -----")
+    manual(removed_more, current)
+    continue <- menu(c("y", "n", "discard"), title = "Continue removing errors?")
+  }
+  if (continue != 3){
+    df_corrected_more <- rbind(df_corrected_more, current[, -c(5:6)])
+  } else if (continue == 3){
+    removed_more$errorType[i] <- "Exclude"
+    removed_more$errorRowNr[i] <- "Exclude the trial (PitchError more than 5% (3 notes))"
   }
 }
 
-# Detect errorType and errorRowNr
-for (i in 1:nrow(df_removed_onset)){
-  current <- df_onset %>% dplyr::filter(SubNr == df_removed_onset[i,1] & BlockNr == df_removed_onset[i,2] & TrialNr == df_removed_onset[i,3])
+# manual addition for missing notes
+print("----- Add NA to missing notes -----")
+df_removed_onset_less <- subset(df_removed_onset, df_removed_onset$errorType == "Less")
+df_rescued_onset_less <- data.frame()
+for (i in 1:nrow(df_removed_onset_updated)){
+  current <- df_onset %>% dplyr::filter(SubNr == df_removed_onset_errorType[i,1] & BlockNr == df_removed_onset_updated[i,2] & TrialNr == df_removed_onset_updated[i,3])
   current$RowNr <- c(1:nrow(current))
-  manual(current, df_ideal)
+  length_diff <- diff_length(current$Pitch, df_ideal$Pitch)
+  table <- data.frame("RowNr" = c(1:length(df_ideal$Pitch)))
+  table$Observed <- fill_by_na(current$Pitch)
+  table$Ideal <- df_ideal$Pitch
+  table$Diff[table$Observed == table$Ideal] <- 0
+  table$Diff[table$Observed != table$Ideal] <- 1
+  print(table)
+  graph <- ggplot() +
+    geom_line(data = table, aes(x = RowNr, y = Observed), colour = "#F8766D") +
+    geom_line(data = table, aes(x = RowNr, y = Ideal), colour = "#00BFC4") +
+    geom_point(data = table, aes(x = RowNr, y = Observed), colour = "#F8766D") +
+    geom_point(data = table, aes(x = RowNr, y = Ideal), colour = "#00BFC4") +
+    scale_x_continuous("RowNr", table$RowNr) +
+    coord_fixed(ratio = 1/5) +
+    labs(title = sprintf("SubNr: %i, BlockNr: %i, TrialNr: %i", df_removed_onset_updated[i,1], df_removed_onset_updated[i,2], df_removed_onset_updated[i,3]), y = "Pitch")
+  print(graph)
+  corrected <- editData(current, viewer = "pane")
+  df_rescued_onset_less <- rbind(df_rescued_onset_less, corrected)
 }
 
-# manual removal
-for (i in 1:nrow(df_removed_onset)){
-  current <- df_onset %>% dplyr::filter(SubNr == df_removed_onset[i,1] & BlockNr == df_removed_onset[i,2] & TrialNr == df_removed_onset[i,3])
+# Insert NA rows
+print("----- Inspect data -----")
+df_removed_onset_equal <- subset(df_removed_onset, df_removed_onset$errorType == "Equal")
+df_rescued_onset_equal <- data.frame()
+for (i in 1:nrow(df_removed_onset_equal)){
+  current <- df_onset %>% dplyr::filter(SubNr == df_removed_onset_equal[i,1] & BlockNr == df_removed_onset_equal[i,2] & TrialNr == df_removed_onset_equal[i,3])
   current$RowNr <- c(1:nrow(current))
-  editData(current, viewer = "pane")
+  table <- data.frame("RowNr" = c(1:length(df_ideal$Pitch)))
+  table$Observed <- current$Pitch
+  table$Ideal <- df_ideal$Pitch
+  table$Diff[table$Observed == table$Ideal] <- 0
+  table$Diff[table$Observed != table$Ideal] <- 1
+  print(table)
+  graph <- ggplot() +
+    geom_line(data = table, aes(x = RowNr, y = Observed), colour = "#F8766D") +
+    geom_line(data = table, aes(x = RowNr, y = Ideal), colour = "#00BFC4") +
+    geom_point(data = table, aes(x = RowNr, y = Observed), colour = "#F8766D") +
+    geom_point(data = table, aes(x = RowNr, y = Ideal), colour = "#00BFC4") +
+    scale_x_continuous("RowNr", table$RowNr) +
+    coord_fixed(ratio = 1/5) +
+    labs(title = sprintf("SubNr: %i, BlockNr: %i, TrialNr: %i", df_removed_onset_equal[i,1], df_removed_onset_equal[i,2], df_removed_onset_equal[i,3]), y = "Pitch")
+  print(graph)
+  corrected <- editData(current, viewer = "pane")
+  df_rescued_onset_equal <- rbind(df_rescued_onset_equal, corrected)
 }
+
+# insert NA row and then edit dataframe
 
 ####################################
 # Export csv files
