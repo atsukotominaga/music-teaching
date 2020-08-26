52e6043427e978a1729203a6edb0e3ad42d2ea4b
Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>
2020-08-26 14:37:53 +0200

    fix indent problem
    Signed-off-by: Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>

--- script/R/preprocessor/function.R
+++ script/R/preprocessor/function.R
@@ -1,34 +1,33 @@
+# install and load required packages
+if (!require("data.table")) {install.packages("data.table"); require("data.table")}
+if (!require("editData")) {install.packages("editData"); require("editData")}
+
 # check whether there are pitch errors
+
 
 checker <- function(data, ideal){
   dt_errors <- data.table() # return data of erroneous trials - SubNr/BlockNr/TrialNr/Reason
-  colnames(dt_errors) <- c("SubNr", "BlockNr", "TrialNr", "Reason")
   for (subject in unique(data$SubNr)){
-    print(sprintf("---Subject %i---", subject)){
-      for (block in c(1:4)){
-        for (trial in c(1:8)){
-          current <- data[SubNr == subject & BlockNr == block & TrialNr == trial]
-          if (nrow(current) != 0){ # if the current trial is not empty
-            if (nrow(ideal) != nrow(current)){ # if there is an extra/missing note
-              dt_error <- rbind(dt_error, data.table(subject, block, trial, "NoteNr error"))
-            } else if (nrow(ideal) == nrow(current)){ # if there is a wrong substituted note
-                boo = TRUE # terminate a loop once the first error was detected
-                for (note in 1:nrow(ideal)){
-                  # detect error
-                  if (current$Pitch[note] != ideal$Pitch[note]){
-                    dt_error <- rbind(dt_error, data.table(subject, block, trial, paste("Pitch error - RowNr ", as.character(note), sep = "")))
-                    boo = FALSE
-                    if (boo == FALSE){
-                      break
-                    }
-                  }
-                }
+    for(block in c(1:4)){
+      for (trial in c(1:8)){
+        current <- data[SubNr == subject & BlockNr == block & TrialNr == trial]
+        if (nrow(current) != 0){ # current data not empty
+          if (nrow(current) != nrow(ideal)){ # extra/missing note
+            dt_errors <- rbind(dt_errors, data.table(subject, block, trial, "NoteNr error")) 
+          } else if (nrow(current) == nrow(ideal)){ # substituted note
+            for (note in 1:nrow(ideal)){
+              if (current$Pitch[note] != ideal$IdealPerformance[note]){
+                dt_errors <- rbind(dt_errors, data.table(subject, block, trial, paste("Pitch error - RowNr ", as.character(note), sep = "")))
+                break
+              }
             }
-          } else if (nrow(current) == 0){ # if the current trial is empty
-            dt_error <- rbind(dt_error, data.table(subject, block, trial, "Missing trial"))
           }
+        } else if (nrow(current) == 0){ # current data empty
+          dt_errors <- rbind(dt_errors, data.table(subject, block, trial, paste("Missing trial")))
         }
       }
     }
-  } return(df_error) # return erroneous trials
+  }
+  colnames(dt_errors) <- c("SubNr", "BlockNr", "TrialNr", "Reason")
+  return(dt_errors)
 }
