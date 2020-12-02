e7337ff02958f3f791d9c9d709c6b826916a5a9b
Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>
2020-12-02 20:11:26 +0100

    removed old analysis and update error_summary
    Signed-off-by: Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>

--- script/R/analysis/export.R
+++
@@ -1,6 +0,0 @@
-# export R scripts from Rmd
-knitr::purl("../../RNotebook/ioi_1.Rmd")
-knitr::purl("../../RNotebook/ioi_3.Rmd")
-knitr::purl("../../RNotebook/articulation.Rmd")
-knitr::purl("../../RNotebook/dynamics.Rmd")
-knitr::purl("../../RNotebook/questionnaire.Rmd")
