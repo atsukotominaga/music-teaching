81e238bb70d44e2eba53b5488b9cec055d18c056
Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>
2022-07-07 22:46:19 +0200

    edit x/y axis labels
    Signed-off-by: Atsuko Tominaga <Tominaga_Atsuko@phd.ceu.edu>

--- experiment-1/analysis/stats/variability.R
+++ experiment-1/analysis/stats/variability.R
@@ -42,12 +42,14 @@ ggline(ioi_dyn_seq_each, x = "Interval", y = "Mean", add = "mean_se", shape = "C
 ioi_cv_trial <- dt_ioi[, .(N = .N, Mean = mean(IOI), SD = sd(IOI), CV = sd(IOI)/mean(IOI)), by = .(SubNr, Condition, Skill, BlockNr, TrialNr)]
 ioi_cv_subject <- ioi_cv_trial[, .(N = .N, MeanCV = mean(CV), SD = sd(CV)), by = .(SubNr, Condition, Skill)]
 ioi_cv_subject <- ioi_cv_subject[order(SubNr, Condition, Skill)] # ordering
-ioi_cv_subject
+ioi_cv <- ioi_cv_subject[, .(N = .N, MeanCV = mean(MeanCV), MedianCV = median(MeanCV), SD = sd(MeanCV), IQR = IQR(MeanCV)), by = .(Condition, Skill)]
+ioi_cv
 
 ggplot(ioi_cv_subject, aes(x = Condition, y = MeanCV, color = Condition)) +
   geom_boxplot() +
   geom_line(aes(group = SubNr), size = 0.4, color = "gray") +
   geom_point(aes(color = Condition)) +
+  labs(y = "CVs") +
   facet_grid(. ~ Skill) +
   theme_pubr()
 
@@ -58,8 +60,13 @@ diff <- ioi_cv_subject[Condition == "teaching" & Skill == "articulation"]$MeanCV
 ioi_art_cv_norm <- shapiro.test(diff)
 ioi_art_cv_norm
 
+# wilcoxon test
 ioi_art_cv_wilcoxon <- wilcox.test(MeanCV ~ Condition, data = ioi_cv_subject[Skill == "articulation"], paired = TRUE, alternative = "two.sided")
 ioi_art_cv_wilcoxon
+
+# effect size
+wilcoxeff_art_cv <- ioi_cv_subject[Skill == "articulation"] %>% wilcox_effsize(MeanCV ~ Condition, paired = TRUE)
+wilcoxeff_art_cv
 
 
 ## ---- echo = FALSE----------------------------------------
@@ -68,19 +75,27 @@ diff2 <- ioi_cv_subject[Condition == "teaching" & Skill == "dynamics"]$MeanCV-io
 ioi_dyn_cv_norm <- shapiro.test(diff2)
 ioi_dyn_cv_norm
 
+# wilcoxon test
 ioi_dyn_cv_wilcoxon <- wilcox.test(MeanCV ~ Condition, data = ioi_cv_subject[Skill == "dynamics"], paired = TRUE, alternative = "two.sided")
 ioi_dyn_cv_wilcoxon
+
+# effect size
+wilcoxeff_dyn_cv <- ioi_cv_subject[Skill == "dynamics"] %>% wilcox_effsize(MeanCV ~ Condition, paired = TRUE)
+wilcoxeff_dyn_cv
 
 
 ## ---- echo = FALSE----------------------------------------
 ioi_tra_trial <- dt_ioi[Subcomponent == "LtoS" | Subcomponent == "StoL" | Subcomponent == "FtoP" | Subcomponent == "PtoF", .(N = .N, Mean = mean(IOI), SD = sd(IOI)), by = .(SubNr, Condition, Skill, BlockNr, TrialNr)]
 ioi_tra_subject <- ioi_tra_trial[, .(N = .N, Mean = mean(Mean), SD = sd(Mean)), by = .(SubNr, Condition, Skill)]
 ioi_tra_subject <- ioi_tra_subject[order(SubNr, Condition, Skill)] # ordering
+ioi_tra <- ioi_tra_subject[, .(N = .N, Mean = mean(Mean), Median = median(Mean), SD = sd(Mean), IQR = IQR(Mean)), by = .(Condition, Skill)]
+ioi_tra
 
 ggplot(ioi_tra_subject, aes(x = Condition, y = Mean, color = Condition)) +
   geom_boxplot() +
   geom_line(aes(group = SubNr), size = 0.4, color = "gray") +
   geom_point(aes(color = Condition)) +
+  labs(y = "IOIs (ms)") +
   facet_grid(. ~ Skill) +
   theme_pubr()
 
@@ -91,8 +106,13 @@ diff3 <- ioi_tra_subject[Condition == "teaching" & Skill == "articulation"]$Mean
 ioi_art_tra_norm <- shapiro.test(diff3)
 ioi_art_tra_norm
 
+# wilcoxon test
 ioi_art_tra_wilcoxon <- wilcox.test(Mean ~ Condition, data = ioi_tra_subject[Skill == "articulation"], paired = TRUE, alternative = "two.sided")
 ioi_art_tra_wilcoxon
+
+# effect size
+wilcoxeff_art_tra <- ioi_tra_subject[Skill == "articulation"] %>% wilcox_d(Mean ~ Condition, paired = TRUE)
+wilcoxeff_art_tra
 
 
 ## ---- echo = FALSE----------------------------------------
@@ -101,8 +121,17 @@ diff4 <- ioi_tra_subject[Condition == "teaching" & Skill == "dynamics"]$Mean-ioi
 ioi_dyn_tra_norm <- shapiro.test(diff4)
 ioi_dyn_tra_norm
 
+# t test
+ioi_dyn_tra_t_test <- t.test(Mean ~ Condition, data = ioi_tra_subject[Skill == "dynamics"], paired = TRUE, alternative = "two.sided")
+ioi_dyn_tra_t_test
+
+# wilcoxon test
 ioi_dyn_tra_wilcoxon <- wilcox.test(Mean ~ Condition, data = ioi_tra_subject[Skill == "dynamics"], paired = TRUE, alternative = "two.sided")
 ioi_dyn_tra_wilcoxon
+
+# effect size
+wilcoxeff_dyn_tra <- ioi_tra_subject[Skill == "dynamics"] %>% wilcox_effsize(Mean ~ Condition, paired = TRUE)
+wilcoxeff_dyn_tra
 
 
 ## ----export, include = FALSE------------------------------
